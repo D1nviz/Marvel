fce09b6d5ba967028b12aeba839eadcea67996d2
Dinviz <luckynoob473@gmail.com>
2023-04-13 12:59:48 +0300

    added CharSearch.jsx

--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -4,19 +4,28 @@ import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 import ErrorBoundary from "../errorBoundary/ErrorBoundaty";
+import CharSearch from "../charSearch/CharSearch";
 
 import decoration from '../../resources/img/spiderman.png';
 
 const MainPage = () => {
    const [selectedChar, setChar] = useState(null);
 
-	const onCharSelected = (id) => setChar(id);
+   const onCharSelected = (id) => setChar(id);
    return (
       <>
          <ErrorBoundary> <RandomChar /></ErrorBoundary>
          <div className="char__content">
             <CharList onCharSelected={onCharSelected} />
-            <ErrorBoundary> <CharInfo charId={selectedChar} /></ErrorBoundary>
+            <div>
+               <ErrorBoundary>
+                  <CharInfo charId={selectedChar} />
+               </ErrorBoundary>
+
+               <ErrorBoundary>
+                  <CharSearch />
+               </ErrorBoundary></div>
+
          </div>
          <div className="box-decoration">
             <img className="bg-decoration" src={decoration} alt="vision" />
--- src/services/MarvelService.jsx
+++ src/services/MarvelService.jsx
@@ -7,6 +7,11 @@ const useMarvelService = () => {
 	const _apiKey =`apikey=${process.env.REACT_APP_API_KEY}`;
 	const _baseOffset = 210;
 
+	const getCharacterByName = async (name) => {
+		const res = await request(`${_apiBase}characters?name=${name}&${_apiKey}`);
+		return res.data.results.map(_transformCharacter);
+	};
+
 	const getAllCharacters = async (offset = _baseOffset) => {
 		const res = await request(`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`);
 		return res.data.results.map(_transformCharacter);
@@ -56,7 +61,7 @@ const useMarvelService = () => {
 		}
 	};
 	
-	return { loading, error, getAllCharacters, getCharacter, clearError, getAllComics, getComic}
+	return { loading, error, getAllCharacters, getCharacter, clearError, getAllComics, getComic, getCharacterByName}
 }
 
 export default useMarvelService;
